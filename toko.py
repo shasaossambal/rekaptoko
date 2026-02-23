bcc8b03a0353f2dcd9af2afcb34f991323175851
shasaossambal <159097708+shasaossambal@users.noreply.github.com>
2026-02-23 15:24:37 +0700

    Add files via upload

---
+++ toko.py
@@ -0,0 +1,10 @@
+beli=int(input("Jumlah barang dibeli ="))
+jual=int(input("Jumlah barang terjual ="))
+harbeli=int(input("Harga beli barang ="))
+harjual=int(input("Harga jual barang="))
+sisa= beli-jual
+laba=harjual-harbeli
+latot=laba*jual
+print("Jumlah sisa barang adalah =",sisa)
+print("Jumlah laba tiap barang adalah =",laba)
+print("Jumlah laba total adalah =",latot)
